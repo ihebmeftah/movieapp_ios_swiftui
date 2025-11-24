b8716aeb2e593e0eb6cf30c9fbc2535d42485e05
ihebmeftah <ihebmeftah666@gmail.com>
2025-11-24 19:31:15 +0100

    Initial Commit

---
+++ projectswift/projectswiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectswiftApp.swift
+//  projectswift
+//
+//  Created by Maalej Ahmed on 24/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct projectswiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ projectswiftTests/projectswiftTests.swift
@@ -0,0 +1,17 @@
+//
+//  projectswiftTests.swift
+//  projectswiftTests
+//
+//  Created by Maalej Ahmed on 24/11/2025.
+//
+
+import Testing
+@testable import projectswift
+
+struct projectswiftTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
